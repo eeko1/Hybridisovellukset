eb49a719eceda247fada0174bdd0e4949a28efd0
Eemi <eemi.korhonen@metropolia.fi>
2024-01-24 14:22:19 +0200

    schemas and resolvers modified

--- graphql-server/src/api/resolvers/mediaResolver.ts
+++ graphql-server/src/api/resolvers/mediaResolver.ts
@@ -1,9 +1,21 @@
-import {fetchAllMedia} from '../models/mediaModel';
+import {
+  fetchAllMedia,
+  fetchMediaById,
+  fetchMediaByTag,
+} from '../models/mediaModel';
 
 export default {
   Query: {
     mediaItems: async () => {
       return await fetchAllMedia();
+    },
+    mediaItem: async (_parent: undefined, args: {media_id: string}) => {
+      console.log(args);
+      const id = Number(args.media_id);
+      return await fetchMediaById(id);
+    },
+    mediaItemsByTag: async (_parent: undefined, args: {tag: string}) => {
+      return await fetchMediaByTag(args.tag);
     },
   },
 };
