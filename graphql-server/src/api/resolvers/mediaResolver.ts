e6bcbe2b9322955ad801dfdcb4301f06549c39ac
Eemi <eemi.korhonen@metropolia.fi>
2024-01-26 09:18:10 +0200

    resolvers fixed

--- graphql-server/src/api/resolvers/mediaResolver.ts
+++ graphql-server/src/api/resolvers/mediaResolver.ts
@@ -1,8 +1,14 @@
+import {MediaItem} from '@sharedTypes/DBTypes';
 import {
   fetchAllMedia,
   fetchMediaById,
   fetchMediaByTag,
+  postMedia,
+  postTagToMedia,
+  putMedia,
 } from '../models/mediaModel';
+import {MyContext} from '../../local-types';
+import {GraphQLError} from 'graphql';
 
 export default {
   Query: {
@@ -10,12 +16,44 @@ export default {
       return await fetchAllMedia();
     },
     mediaItem: async (_parent: undefined, args: {media_id: string}) => {
-      console.log(args);
       const id = Number(args.media_id);
       return await fetchMediaById(id);
     },
     mediaItemsByTag: async (_parent: undefined, args: {tag: string}) => {
       return await fetchMediaByTag(args.tag);
+    },
+  },
+  Mutation: {
+    createMediaItem: async (
+      _parent: undefined,
+      args: {input: Omit<MediaItem, 'media_id' | 'created_at' | 'thumbnail'>},
+      context: MyContext,
+    ) => {
+      if (!context.user || !context.user.user_id) {
+        throw new GraphQLError('Not authorized', {
+          extensions: {code: 'NOT_AUTHORIZED'},
+        });
+      }
+      return postMedia(args.input);
+    },
+    addTagToMediaItem: async (
+      _parent: undefined,
+      args: {input: {media_id: string; tag_name: string}},
+    ) => {
+      console.log(args);
+      return await postTagToMedia(
+        args.input.tag_name,
+        Number(args.input.media_id),
+      );
+    },
+    updateMediaItem: async (
+      _parent: undefined,
+      args: {
+        input: Pick<MediaItem, 'title' | 'description'>;
+        media_id: string;
+      },
+    ) => {
+      return await putMedia(args.input, Number(args.media_id));
     },
   },
 };
